b611f0a0f8f0a5487c6aa9bfd59e8323bf63d67a
KosarevMaxim <kosarev.maxim99@gmail.com>
2022-12-14 16:07:10 +0300

    v1

--- main.py
+++ main.py
@@ -1,4 +1,132 @@
 from fastapi import FastAPI
+import os
+import pathlib
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import io
+import scipy.misc
+import numpy as np
+from six import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from six.moves.urllib.request import urlopen
+
+import tensorflow as tf
+import tensorflow_hub as hub
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.utils import ops as utils_ops
+
+tf.get_logger().setLevel('ERROR')
+
+COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
+ (0, 2),
+ (1, 3),
+ (2, 4),
+ (0, 5),
+ (0, 6),
+ (5, 7),
+ (7, 9),
+ (6, 8),
+ (8, 10),
+ (5, 6),
+ (5, 11),
+ (6, 12),
+ (11, 12),
+ (11, 13),
+ (13, 15),
+ (12, 14),
+ (14, 16)]
+
+PATH_TO_LABELS = './models/research/object_detection/data/mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+def load_image_into_numpy_array(path):
+  """Load an image from file into a numpy array.
+
+  Puts image into numpy array to feed into tensorflow graph.
+  Note that by convention we put it into a numpy array with shape
+  (height, width, channels), where channels=3 for RGB.
+
+  Args:
+    path: the file path to the image
+
+  Returns:
+    uint8 numpy array with shape (img_height, img_width, 3)
+  """
+  image = None
+  if(path.startswith('http')):
+    response = urlopen(path)
+    image_data = response.read()
+    image_data = BytesIO(image_data)
+    image = Image.open(image_data)
+  else:
+    image_data = tf.io.gfile.GFile(path, 'rb').read()
+    image = Image.open(BytesIO(image_data))
+
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (1, im_height, im_width, 3)).astype(np.uint8)
+
+model_handle = 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1'
+print('loading model...')
+hub_model = hub.load(model_handle)
+print('model loaded!')
+
+image_path1 = '/content/нейросеть.jpg'
+image_np = load_image_into_numpy_array(image_path3)
+flip_image_horizontally = False #@param {type:"boolean"}
+convert_image_to_grayscale = False #@param {type:"boolean"}
+
+# Flip horizontally
+if(flip_image_horizontally):
+  image_np[0] = np.fliplr(image_np[0]).copy()
+
+# Convert image to grayscale
+if(convert_image_to_grayscale):
+  image_np[0] = np.tile(
+    np.mean(image_np[0], 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+plt.figure(figsize=(24,32))
+plt.imshow(image_np[0])
+plt.show()
+
+
+label_id_offset = 0
+image_np_with_detections = image_np.copy()
+
+# Use keypoints if available in detections
+keypoints, keypoint_scores = None, None
+if 'detection_keypoints' in result:
+  keypoints = result['detection_keypoints'][0]
+  keypoint_scores = result['detection_keypoint_scores'][0]
+
+viz_utils.visualize_boxes_and_labels_on_image_array(
+      image_np_with_detections[0],
+      result['detection_boxes'][0],
+      (result['detection_classes'][0] + label_id_offset).astype(int),
+      result['detection_scores'][0],
+      category_index,
+      use_normalized_coordinates=True,
+      max_boxes_to_draw=200,
+      min_score_thresh=.30,
+      agnostic_mode=False,
+      keypoints=keypoints,
+      keypoint_scores=keypoint_scores,
+      keypoint_edges=COCO17_HUMAN_POSE_KEYPOINTS)
+
+plt.figure(figsize=(24,32))
+plt.imshow(image_np_with_detections[0])
+plt.show()
+
+
+results = hub_model(image_np)
+
+# different object detection models have additional results
+# all of them are explained in the documentation
+result = {key:value.numpy() for key,value in results.items()}
+print(result.keys())
 
 app = FastAPI()
 
